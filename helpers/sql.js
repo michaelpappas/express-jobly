98e07c3cb5303f100cc2211b8fec29b34ba40b4c
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-01-11 11:31:05 -0800

    add docstring to sqlForPartialUpdate

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,6 +1,21 @@
 const { BadRequestError } = require("../expressError");
 
-// TODO: THIS NEEDS SOME GREAT DOCUMENTATION.
+/**
+ *  Takes two inputs: dataToUpdate and jsToSql
+ *    - dataToUpdate: An object with key value pairs of columns we want to update
+ *    - jsToSql: Converts camelCase to snakeCase to match db column names
+ *
+ *  Returns an object with setCols and values keys
+ *    - setCols: It's a string representing the the variables and the indexes of
+ *               the prepared statement.
+ *    - values:  The actual data to insert into the prepared statement.
+ *
+ *  Example return: {
+      setCols: '"handle"=$1, "name"=$2, "logo_url"=$3, "description"=$4, "num_employees"=$5',
+      values: [ 'new', 'New', 'http://new.img', 'DescNew', 10 ]
+    }
+ *
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
