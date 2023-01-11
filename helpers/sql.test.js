fbec15a2df53a8b921a94a986cb28b5247b06305
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-01-11 11:42:35 -0800

    add test for our sqlForPartialUpdate helper function

---
+++ helpers/sql.test.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const { sqlForPartialUpdate } = require("./sql");
+
+describe("testSqlForPartialUpdate", function () {
+  const data = {
+    username: "new",
+    firstName: "Test",
+    lastName: "Tester",
+  };
+
+  const jsToSql = {
+    username: "username",
+    firstName: "first_name",
+    lastName: "last_name",
+  };
+
+  test("correct result on three keys", function () {
+    const result = sqlForPartialUpdate(data, jsToSql);
+    const expectedResult = {
+      setCols: '"username"=$1, "first_name"=$2, "last_name"=$3',
+      values: ['new', 'Test', 'Tester']
+    }
+    expect(result).toEqual(expectedResult);
+  });
+});
