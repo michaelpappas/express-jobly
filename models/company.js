c6c4e874032796250239392fff75cecf3d943ac7
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-01-11 16:13:16 -0800

    finish up to part 2, before code review

--- models/company.js
+++ models/company.js
@@ -18,16 +18,16 @@ class Company {
 
   static async create({ handle, name, description, numEmployees, logoUrl }) {
     const duplicateCheck = await db.query(
-        `SELECT handle
+      `SELECT handle
            FROM companies
            WHERE handle = $1`,
-        [handle]);
+      [handle]);
 
     if (duplicateCheck.rows[0])
       throw new BadRequestError(`Duplicate company: ${handle}`);
 
     const result = await db.query(
-        `INSERT INTO companies(
+      `INSERT INTO companies(
           handle,
           name,
           description,
@@ -36,13 +36,13 @@ class Company {
            VALUES
              ($1, $2, $3, $4, $5)
            RETURNING handle, name, description, num_employees AS "numEmployees", logo_url AS "logoUrl"`,
-        [
-          handle,
-          name,
-          description,
-          numEmployees,
-          logoUrl,
-        ],
+      [
+        handle,
+        name,
+        description,
+        numEmployees,
+        logoUrl,
+      ],
     );
     const company = result.rows[0];
 
@@ -56,7 +56,7 @@ class Company {
 
   static async findAll() {
     const companiesRes = await db.query(
-        `SELECT handle,
+      `SELECT handle,
                 name,
                 description,
                 num_employees AS "numEmployees",
@@ -68,23 +68,27 @@ class Company {
 
   /** Filter all companies.
    *
+   * Will throw an error if minEmployees is greater than maxEmployees.
+   *
    * Returns [{ handle, name, description, numEmployees, logoUrl }, ...]
    * */
 
   static async filterAll(filters) {
 
+    const { minEmployees, maxEmployees } = filters;
 
+    if (minEmployees > maxEmployees) {
+      throw new BadRequestError("minEmployees cannot be greater than maxEmployees");
+    }
 
-    const {setCols, values} = sqlForFilter(
+    const { setCols, values } = sqlForFilter(
       filters,
       {
         minEmployees: "num_employees",
         maxEmployees: "num_employees",
         nameLike: "name"
       }
-    )
-
-    console.log("setCols=", setCols)
+    );
 
     const querySql = `SELECT handle,
           name,
@@ -97,10 +101,7 @@ class Company {
 
 
     const companiesRes = await db.query(
-        querySql, values);
-
-    console.log("OUR SQL STATEMENT", querySql)
-    console.log("OUR VALUES=", values)
+      querySql, values);
 
 
     return companiesRes.rows;
@@ -118,14 +119,14 @@ class Company {
 
   static async get(handle) {
     const companyRes = await db.query(
-        `SELECT handle,
+      `SELECT handle,
                 name,
                 description,
                 num_employees AS "numEmployees",
                 logo_url AS "logoUrl"
            FROM companies
            WHERE handle = $1`,
-        [handle]);
+      [handle]);
 
     const company = companyRes.rows[0];
 
@@ -148,11 +149,11 @@ class Company {
 
   static async update(handle, data) {
     const { setCols, values } = sqlForPartialUpdate(
-        data,
-        {
-          numEmployees: "num_employees",
-          logoUrl: "logo_url",
-        });
+      data,
+      {
+        numEmployees: "num_employees",
+        logoUrl: "logo_url",
+      });
     const handleVarIdx = "$" + (values.length + 1);
 
     const querySql = `
@@ -175,21 +176,16 @@ class Company {
 
   static async remove(handle) {
     const result = await db.query(
-        `DELETE
+      `DELETE
            FROM companies
            WHERE handle = $1
            RETURNING handle`,
-        [handle]);
+      [handle]);
     const company = result.rows[0];
 
     if (!company) throw new NotFoundError(`No company: ${handle}`);
   }
 }
-// async function testFilter(){
-//   const testFilter = await Company.filterAll({nameLike: ''})
-//   console.log(testFilter)
-// }
-// testFilter()
 
 
 module.exports = Company;
